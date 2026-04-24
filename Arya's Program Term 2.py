7eead986832e549077f313cd4adc6ca41e49bda9
Aryastech <23apanniarselvam@wakatipu.school.nz>
2026-04-24 15:00:49 +1200

    Finished order_type function that give the user the option to pickup or delivery

--- Arya's Program Term 2.py
+++ Arya's Program Term 2.py
@@ -13,15 +13,25 @@ def drink_size():
             print("Invalid input Please enter a valid number")   
 
 def order_type():
-    print("")
-
-
-
-
-
+    print("Order for 1. Pickup or 2. Delivery") 
+    while True:
+        try:
+            choice_order_type = int(input("Enter your option")) 
+            if choice_order_type in [1, 2,]:   
+                break 
+            else:
+                print("Invalid choice Please enter a number between 1 or2")    
+        except ValueError:
+            print("Invalid input Please enter a valid number")
+        if choice_order_type == 1:
+            print("Pickup is free and should be ready in 5-10 minites")
+            drink_size()
+        if choice_order_type == 1: 
+            print("Delivery is $3.49 , Orders talke 5-10 minites after order")
+            drink_size()
+        
 def menu():
-    print("Welcome to Dash Drink ") 
-
-
+    print("welcome to Dash Drink")
+    order_type()
 
 menu()
